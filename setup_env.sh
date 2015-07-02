65e610ad9a2d7331c6bee3ad5c8b212cd9be200a
David Hatch <david.hatch@mongodb.com>
2015-07-02 10:44:21 -0400

    fix.

--- setup_env.sh
+++ setup_env.sh
@@ -3,16 +3,20 @@ echo "Building..."
 
 ORIG_DIR=`pwd`
 
-cd mongo && scons -j4 mongod mongo && echo "Mongo built." && cd $ORIG_DIR
+cd mongo;
+scons -j4 mongod mongo || cd $ORIG_DIR && exit $?
+echo "Mongo built." && cd $ORIG_DIR
 
 cd mongo-c-driver
 export CC='gcc-5'
 
 if [! -f configure]; then
 ./autogen.sh --with-libbson=bundled --enable-debug-symbols=full --enable-optimizations=no
+./src/libbson/autogen.sh --enable-debug-symbols=full --enable-optimizations=no
 fi
 
-make -j4 all && echo "c driver built."
+make -j4 all || cd $ORIG_DIR && exit $?
+echo "c driver built."
 
 cd $ORIG_DIR
 
